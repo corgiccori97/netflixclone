824f02fbf306dff5ea4bdf225ab7e8736c99a0dd
corgiccori97 <tkdgmlgg7@gmail.com>
2023-03-07 05:02:15 +0900

    a?

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "aa33a897bf3e30adf7eaf07f28d9fa85";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+interface IMovie {
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
+    title: string;
+    overview: string;
+}
+
+export interface IGetMoviesResult {
+    dates: {
+    maximum: string;
+    minimum: string;
+    };
+    page: number;
+    results: IMovie[];
+    total_pages: number;
+    total_results: number;
+}
+
 export function getMovies() {
     return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then((response) => response.json());
 }
